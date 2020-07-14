95db331230be8c465f4e5ddcc23fb1e2f3b968ad
EC2 Default User <ec2-user@ip-10-0-0-24.ap-northeast-1.compute.internal>
2020-07-14 12:39:14 +0000

    fix

--- scripts/send_ip.sh
+++ scripts/send_ip.sh
@@ -5,8 +5,11 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 . ${SCRIPT_DIR}/lib.sh
 
 
-IPADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 
+
+upsert_domain
+
+IPADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 CONTENT="serverが起動しました"
 TITLE="${DOMAIN_NAME} (${IPADDRESS})"
 DESCRIPTION="Pass: ${PASS}"
